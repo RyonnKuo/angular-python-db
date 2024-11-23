6eba50a7a031d3d8ec2e22eed76cc520c7ad090d
RyonnKuo <125716399+RyonnKuo@users.noreply.github.com>
2024-11-23 22:54:12 +0800

    Update python-api.service.ts

--- src/app/service/python-api.service.ts
+++ src/app/service/python-api.service.ts
@@ -34,4 +34,116 @@ export class PythonApiService {
       console.log(`[py api] studentReg error: ${JSON.stringify(error)}`);
     }
   }
+
+  async getAllStudent(): Promise<any> {
+
+    try {
+      const res = await fetch('http://127.0.0.1:5000/api/get_all_student', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          "Content-Type": "application/json",
+        },
+      });
+
+      const result = await res.json();
+      console.log(`[py api] getAllStudent result: ${JSON.stringify(result)}`)
+      return {
+        "success": true,
+        "result": result
+      }
+    } catch (error) {
+      return {
+        "success": false,
+        "result": `[py api] getAllStudent error: ${JSON.stringify(error)}`
+      };
+    }
+  }
+
+  async getStudent(_email: string): Promise<any> {
+    try {
+      const param = {
+        email: _email
+      }
+      const res = await fetch('http://127.0.0.1:5000/api/get_student', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(param)
+      });
+
+      const result = await res.json();
+      console.log(`[py api] getStudent result: ${JSON.stringify(result)}`)
+      return {
+        "success": true,
+        "result": result
+      }
+    } catch (error) {
+      return {
+        "success": false,
+        "result": `[py api] getStudent error: ${JSON.stringify(error)}`
+      };
+    }
+  }
+
+
+  async getAllAdmin(): Promise<any> {
+
+    try {
+      const res = await fetch('http://127.0.0.1:5000/api/get_all_admin', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          "Content-Type": "application/json",
+        },
+      });
+
+      const result = await res.json();
+      console.log(`[py api] getAllAdmin result: ${JSON.stringify(result)}`)
+      return {
+        "success": true,
+        "result": result
+      }
+    } catch (error) {
+      return {
+        "success": false,
+        "result": `[py api] getAllAdmin error: ${JSON.stringify(error)}`
+      };
+    }
+  }
+
+  async getAdmin(_email: string): Promise<any> {
+    try {
+      const param = {
+        email: _email
+      }
+      const res = await fetch('http://127.0.0.1:5000/api/get_admin', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(param)
+      });
+
+      const result = await res.json();
+      console.log(`[py api] getStudent result: ${JSON.stringify(result)}`);
+      return {
+        "success": true,
+        "result": result
+      }
+    } catch (error) {
+      return {
+        "success": false,
+        "result": `[py api] getAdmin error: ${JSON.stringify(error)}`
+      };
+    }
+  }
+
 }
