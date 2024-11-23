9526709824964c1179bfbff285234a88b731b6bd
RyonnKuo <125716399+RyonnKuo@users.noreply.github.com>
2024-11-23 22:54:45 +0800

    Update global.service.ts

--- src/app/service/global.service.ts
+++ src/app/service/global.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject, BehaviorSubject, Observable } from 'rxjs';
 import { LoginType } from './ipssdk.service'
+import { PythonApiService } from './python-api.service';
 
 
 @Injectable({
@@ -9,57 +10,16 @@ import { LoginType } from './ipssdk.service'
 
 export class GlobalService {
 
-  private static _instance: GlobalService;
-
-  static get instance(): GlobalService {
-    if (!GlobalService._instance) GlobalService._instance = new GlobalService();
-    return GlobalService._instance;
-  }
-
   private loginSubject: BehaviorSubject<LoginType>;
   checkLoginObservable: Observable<LoginType>;
   validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   private _loginType: LoginType = LoginType.Non;
+  private _user: any;
 
-  tutorAccountList = [
-    {
-      account: 'tutor1@test',
-      password: '123'
-    },{
-      account: 'tutor2@test',
-      password: '123'
-    },{
-      account: 'tutor3@test',
-      password: '123'
-    }
-  ];
-  adminAccountList = [
-    {
-      account: 'admin1@test',
-      password: '123'
-    },{
-      account: 'admin2@test',
-      password: '123'
-    },{
-      account: 'admin3@test',
-      password: '123'
-    }
-  ]
-  studentAccountList = [
-    {
-      account: 'student1@test',
-      password: '123'
-    },{
-      account: 'student2@test',
-      password: '123'
-    },{
-      account: 'student3@test',
-      password: '123'
-    }
-  ];
 
   constructor(
+    public pas: PythonApiService,
   ) {
     this.loginSubject = new BehaviorSubject<LoginType>(this.loginType);
     this.checkLoginObservable = this.loginSubject.asObservable();
@@ -67,6 +27,10 @@ export class GlobalService {
 
   get loginType(): LoginType {
     return this._loginType;
+  }
+
+  get currentUser() {
+    return this._user;
   }
 
   async testCallBackend(): Promise<any> {
@@ -86,32 +50,73 @@ export class GlobalService {
     return resArr.filter(v => v !== '' && v !== null); //remove: null, NaN, false, ""
   }
 
-  private validateMember(account: string, password: string) {
-    // 送資料到DB找對應的帳號密碼
 
-    const inputAccount = account;
-    const inputPassword = password;
-
-
-
-    console.log(`[gos] validateMember get login type: ${this._loginType}`);
-    return this._loginType;
-  }
 
   validateEmail(email: string) {
     return email.match(this.validRegex);
   }
 
-  getUniqId(email: string) {
-    // 問DB拿對應資料表(Student, Tutor, Manager)資料，根據資料長度給予ID
-  }
-
   async updateLoginStatus(account: string, password: string) {
 
-    const validateRes = await this.validateMember(account, password);
+    const inputAccount = account;
+    const inputPassword = password; // phone number
 
-    if (validateRes) {
-      this.loginSubject.next(validateRes);
+    // 送資料到DB找對應的帳號密碼
+    const getAllStudentResult = await this.pas.getAllStudent();
+    const getAllAdminResult = await this.pas.getAllAdmin();
+
+
+    if (getAllStudentResult.success) {
+      type student = [number, string, string, string]
+      let studentData = getAllStudentResult.result;
+      const formattedStudentData = studentData.map(([sid, name, email, phone]: student) => ({
+        sid,
+        name,
+        email,
+        phone
+      }));
+      // check account is student or not
+      console.log(`[gos] formattedStudentData: ${JSON.stringify(formattedStudentData)}`);
+      const sIdx = formattedStudentData.findIndex((v: { sid: number, name: string, email: string, phone: string }) => v.email == inputAccount);
+      console.log(`[gos] input account is exist in formattedStudentData: ${sIdx}`);
+
+      if (sIdx !== -1) {
+        const accountFitStudent = formattedStudentData[sIdx];
+        if (inputPassword == accountFitStudent.phone) {
+          this._user = accountFitStudent;
+          this.loginSubject.next(LoginType.Student);
+          return true;
+        }
+      }
     }
+
+    if (getAllAdminResult.success) {
+      type admin = [number, number, string, string, string]
+      let adminList = getAllAdminResult.result;
+      const formattedAdminData = adminList.map(([aid, cid, name, email, phone]: admin) => ({
+        aid,
+        cid,
+        name,
+        email,
+        phone
+      }));
+      // check account is admin or not
+      console.log(`[gos] formattedAdminData: ${JSON.stringify(formattedAdminData)}`);
+      const aIdx = formattedAdminData.findIndex((v: { aid: number, cid: number, name: string, email: string, phone: string }) => v.email == inputAccount);
+      console.log(`[gos] input account is exist in formattedAdminData: ${aIdx}`);
+
+      if (aIdx !== -1) {
+        const accountFitAdmin = formattedAdminData[aIdx];
+        if (inputPassword == accountFitAdmin.phone) {
+          this._user = accountFitAdmin;
+          this.loginSubject.next(LoginType.Admin);
+          return true;
+        }
+      }
+    }
+
+    // can not find account from DB
+    this.loginSubject.next(LoginType.Non);
+    return false;
   }
 }
