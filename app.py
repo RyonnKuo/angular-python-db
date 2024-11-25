1d21e32d272690ddfac179d8215dd1f73bcdbfcd
RyonnKuo <125716399+RyonnKuo@users.noreply.github.com>
2024-11-25 23:44:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -65,28 +65,144 @@ def get_all_student():
     print('get_data is called' f"{type(data)}")
     return jsonify(data)
 
-@app.route('/api/get_student', methods=['GET', 'HEAD'])
+@app.route('/api/get_student', methods=['POST', 'GET'])
 def get_student():
     reqData = request.json
     email = reqData.get('email')
     data = Student.get_student(email)
-    print('get_data is called' f"{type(data)}")
+    print('get_student is called' f"{type(data)}")
     return jsonify(data)
 
 # admin
 @app.route('/api/get_all_admin', methods=['GET', 'HEAD'])
 def get_all_admin():
     data = Admin.get_all_admin()
-    print('get_data is called' f"{type(data)}")
+    print('get_all_admin is called' f"{type(data)}")
     return jsonify(data)
 
-@app.route('/api/get_admin', methods=['GET', 'HEAD'])
+@app.route('/api/get_admin', methods=['POST', 'GET'])
 def get_admin():
     reqData = request.json
     email = reqData.get('email')
     data = Admin.get_admin(email)
+    print('get_admin is called' f"{type(data)}")
+    return jsonify(data)
+
+#session
+@app.route('/api/get_all_session', methods=['GET', 'HEAD'])
+def get_all_session():
+    data = Session.get_all_session()
+    print('get_all_session is called' f"{type(data)}")
+    return jsonify(data)
+
+@app.route('/api/update_session', methods=['POST', 'GET'])
+def update_session():
+    if request.method == 'POST':
+      reqData = request.json
+      cid = reqData.get('cid')
+      sdesc = reqData.get('sdesc')
+      enddate = reqData.get('enddate')
+      price = reqData.get('price')
+      startdate = reqData.get('startdate')
+      sname = reqData.get('sname')
+      input = {
+                  'cid': cid,
+                  'sdesc': sdesc,
+                  'enddate': enddate,
+                  'price': price,
+                  'startdate': startdate,
+                  'sname': sname
+          }
+
+      data = Session.update_session(input)
+      print('update_session is called, sid = ' f"{type(data)}")
+      return jsonify(data)
+
+@app.route('/api/delete_session', methods=['POST', 'GET'])
+def delete_session():
+    if request.method == 'POST':
+      reqData = request.json
+      cid = reqData.get('cid')
+      startdate = reqData.get('startdate')
+      sname = reqData.get('sname')
+      input = {
+                  'cid': cid,
+                  'startdate': startdate,
+                  'sname': sname
+          }
+
+      data = Session.delete_session(input)
+      print('delete_session is called, sid = ' f"{type(data)}")
+      return jsonify(data)
+
+@app.route('/api/create_session', methods=['POST', 'GET'])
+def create_session():
+    if request.method == 'POST':
+      reqData = request.json
+      cid = reqData.get('cid')
+      startdate = reqData.get('startdate')
+      sname = reqData.get('sname')
+      sdesc = reqData.get('sdesc')
+      enddate = reqData.get('enddate')
+      price = reqData.get('price')
+      input = {
+                  'cid': cid,
+                  'startdate': startdate,
+                  'sname': sname,
+                  'sdesc': sdesc,
+                  'enddate': enddate,
+                  'price': price
+          }
+
+      data = Session.create_session(input)
+      print('get_enroll is called, sid = ' f"{type(data)}")
+      return jsonify(data)
+
+#enroll
+@app.route('/api/get_all_enroll', methods=['GET', 'HEAD'])
+def get_all_enroll():
+    data = Enroll.get_all_enroll()
     print('get_data is called' f"{type(data)}")
     return jsonify(data)
+
+@app.route('/api/get_enroll', methods=['POST', 'GET'])
+def get_enroll():
+  if request.method == 'POST':
+    reqData = request.json
+    sid = reqData.get('sid')
+    data = Enroll.get_enroll(sid)
+    print('get_enroll is called, sid = ' f"{type(data)}")
+    return jsonify(data)
+
+@app.route('/api/add_enroll', methods=['POST', 'GET'])
+def add_enroll():
+  if request.method == 'POST':
+    reqData = request.json
+    sid = reqData.get('sid')
+    startdate = reqData.get('startdate')
+    sname = reqData.get('sname')
+    tno = reqData.get('tno')
+    coupon = reqData.get('coupon')
+    amount = reqData.get('amount')
+    tdate = reqData.get('tdate')
+    cardid = reqData.get('cardid')
+    cardenddate = reqData.get('cardenddate')
+    cardtype = reqData.get('cardtype')
+    input = {
+              'sid': sid,
+              'startdate': startdate,
+              'sname': sname,
+              'tno': tno,
+              'coupon': coupon,
+              'amount': amount,
+              'tdate': tdate,
+              'cardid': cardid,
+              'cardenddate': cardenddate,
+              'cardtype': cardtype
+          }
+    data = Enroll.add_enroll(input)
+    print('add_enroll is called, sid = ' f"{type(data)}")
+    return jsonify({"success": True, "message": "報名成功！"}), 200
 
 app.secret_key = 'Your Key'
 
--- link.py
+++ link.py
@@ -2,10 +2,18 @@ import psycopg2
 
 connection = psycopg2.connect(
     user='project_1',
-    password='xxxxxx',
+    password='xxxxx',
     host='xxx.xxx.xx.xx',
     port='xxxx',
     dbname='project_1'  # PostgreSQL 的資料庫名稱
 )
 cursor = connection.cursor()
+
+# (
+#     user='project_1',
+#     password='xxxxxx',
+#     host='xxx.xxx.xx.xx',
+#     port='xxxx',
+#     dbname='project_1'
+# )
 
